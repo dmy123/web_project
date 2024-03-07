ab551b34556fa80f9309afc56a7a57c718f75943
dmy_1234 <11991529+dmy_1234@user.noreply.gitee.com>
2024-03-07 16:56:31 +0800

    feat: opt

--- additional/option_patter.go
+++ additional/option_patter.go
@@ -1,1 +1,53 @@
 package additional
+
+import "errors"
+
+type MyStructOption func(m *MyStruct)
+
+type MyStructOptionErr func(m *MyStruct) error
+
+type MyStruct struct {
+	id      uint64
+	name    string
+	address string
+}
+
+//func NewMyStruct(id uint64, name string, opts ...MyStructOption) MyStruct {
+//	res := MyStruct{
+//		id:   id,
+//		name: name,
+//	}
+//	for _, opt := range opts {
+//		opt(&res)
+//	}
+//	return res
+//}
+//
+//func AddAddress(address string) MyStructOption {
+//	return func(m *MyStruct) {
+//		m.address = address
+//	}
+//}
+
+func NewMyStruct1(id uint64, name string, opts ...MyStructOptionErr) (*MyStruct, error) {
+	res := &MyStruct{
+		id:   id,
+		name: name,
+	}
+	for _, opt := range opts {
+		if err := opt(res); err != nil {
+			return res, err
+		}
+	}
+	return res, nil
+}
+
+func AddAddress(address string) MyStructOptionErr {
+	return func(m *MyStruct) error {
+		if address == "" {
+			return errors.New("地址不能为空字符串")
+		}
+		m.address = address
+		return nil
+	}
+}
--- additional/option_patter_test.go
+++ additional/option_patter_test.go
@@ -17,6 +17,21 @@ func TestNewMyStruct(t *testing.T) {
 		want MyStruct
 	}{
 		// TODO: Add test cases.
+		{
+			name: "normal",
+			args: args{
+				id:   1,
+				name: "tom",
+				opts: []MyStructOption{
+					AddAddress("haha"),
+				},
+			},
+			want: MyStruct{
+				id:      1,
+				name:    "tom",
+				address: "haha",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
